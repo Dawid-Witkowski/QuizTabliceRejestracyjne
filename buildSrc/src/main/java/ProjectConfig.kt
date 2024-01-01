7a2ec53df6209c80d6c6322fe37513b3b8f89af1
Dawid Witkowski <dawidwitkowski.dev@gmail.com>
2024-01-01 17:12:28 +0100

    initial project structure set

---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,8 @@
+object ProjectConfig {
+    const val compileSdk = 34
+    const val minSdk = 24
+    const val targetSdk = 34
+    const val versionCode = 5
+    const val versionName = "$versionCode"
+    const val kotlinExtensionVersion = "1.4.3"
+}
